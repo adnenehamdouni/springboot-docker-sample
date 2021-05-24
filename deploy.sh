cfe4393e6c352e03c43f7942677891505dfaf6d3
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2021-05-24 12:21:49 +0200

    update stages snapshot

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker kill spring-boot-docker-sample
-docker rm spring-boot-docker-sample 
+docker kill spring-boot-docker-sample > /dev/null 2>&1
+docker rm spring-boot-docker-sample > /dev/null 2>&1
 docker-compose up -d
